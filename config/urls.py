55f23aef1727eaa70ace22c069285b2dfc813a3f
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-08-10 21:47:45 +0300

    feat: add "admin/" path

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("", views.home ,name="home"),
     path("contacts/", views.contacts ,name="contacts"),
 ]
