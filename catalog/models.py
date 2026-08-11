39173eb7f6ceb0b0b590a23249159999e8649e24
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-08-12 00:59:22 +0300

    fix: add related_name to Product

--- catalog/models.py
+++ catalog/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to="products/", blank=True, null=True)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="products")
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
