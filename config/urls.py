55604d0599009f087e9f9ef3571248c7b03dcf45
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-08-12 00:59:52 +0300

    fix: add MEDIA_URL/ROOT

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,8 @@ urlpatterns = [
     path("contacts/", views.contacts ,name="contacts"),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
