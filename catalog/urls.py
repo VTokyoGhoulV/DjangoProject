a6145f423b2ffcb976b74c150d28ac1b87c05076
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-08-28 04:18:02 +0300

    feat: add product CRUD operations

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from catalog import views as catalog
 
 urlpatterns = [
-    path("", catalog.ProductList.as_view(), name="home"),
-    path("contacts/", catalog.ContactView.as_view(), name="contacts"),
-    path("product_detail/<int:pk>/", catalog.ProductDetail.as_view(), name="product_detail"),
-    path("add_product/", catalog.CreateProduct.as_view(), name="add_product"),
+    path("", catalog.ProductListView.as_view(), name="home"),
+    path("contacts/", catalog.ContactListView.as_view(), name="contacts"),
+    path("product_detail/<int:pk>/", catalog.ProductDetailView.as_view(), name="product_detail"),
+    path("add_product/", catalog.ProductCreateView.as_view(), name="add_product"),
+    path("update_product/<int:pk>/", catalog.ProductUpdateView.as_view(), name="update_product"),
+    path("delete_product/<int:pk>/", catalog.ProductDeleteView.as_view(), name="delete_product"),
 ]
