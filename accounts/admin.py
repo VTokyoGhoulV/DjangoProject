d085238130f121f9307b0d48a35d3f4e60a4a14c
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-09-17 02:44:21 +0300

    feat: add custom user administration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.form import UserRegistrationForm
+from accounts.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = UserRegistrationForm
+
+    list_display = ("email", "is_staff", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {
+            "fields": ("email", "password"),
+        }),
+        ("Профиль", {
+            "fields": ("avatar", "mobile_number", "country"),
+        }),
+        ("Права доступа", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            ),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "avatar",
+                "mobile_number",
+                "country",
+            ),
+        }),
+    )
